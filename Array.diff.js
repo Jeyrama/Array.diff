e635c64f00a0143a6fd3cba6bdc67b3b01d7fbff
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-09-01 21:16:06 -0700

    Update Array.diff.js

--- Array.diff.js
+++ Array.diff.js
@@ -14,3 +14,17 @@ arrayDiff([1,2,2,2,3],[2]) == [1,3]
 
 
 // Solution
+
+function arrayDiff(a, b) {
+  if(b.length==0 || a.length==0)
+    return a;
+  for(let i =0;i<a.length;i++){
+    for(let j=0;j<b.length;j++){
+      if(a[i]==b[j]){
+        a.splice(i,1);
+        i--;
+      }
+    }
+  }
+  return a;
+}
